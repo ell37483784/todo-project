f13dd1b272a3b9db57e9246df72bd9cb0cc816e2
ell <emaslt220100@gmail.com>
2026-04-18 22:49:31 +0600

    final stable todo app

--- todo_project/tasks/views.py
+++ todo_project/tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from .models import Task
 
+
 def task_list(request):
     filter_type = request.GET.get('filter', 'all')
 
@@ -12,14 +13,21 @@ def task_list(request):
     elif filter_type == 'done':
         tasks = tasks.filter(completed=True)
 
+    # добавление задачи
     if request.method == 'POST':
         title = request.POST.get('title')
         if title:
             Task.objects.create(title=title)
-
         return redirect('/')
 
     return render(request, 'tasks/index.html', {'tasks': tasks})
+
+
+def delete_task(request, id):
+    task = Task.objects.filter(id=id).first()
+    if task:
+        task.delete()
+    return redirect('/')
 
 
 def complete_task(request, id):
@@ -27,13 +35,6 @@ def complete_task(request, id):
     if task:
         task.completed = True
         task.save()
-    return redirect('/')
-
-
-def complete_task(request, id):
-    task = get_object_or_404(Task, id=id)
-    task.completed = True
-    task.save()
     return redirect('/')
 
 
