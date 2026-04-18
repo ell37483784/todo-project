97973462de0fbc8f0c4dfaa0f3f86b7aff90f9c6
ell <emaslt220100@gmail.com>
2026-04-18 22:33:27 +0600

    remove auth system and simplify todo app

--- todo_project/tasks/urls.py
+++ todo_project/tasks/urls.py
@@ -4,9 +4,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.task_list),
-    path('register/', views.register),
-    path('login/', LoginView.as_view(template_name='tasks/login.html')),
-    path('logout/', LogoutView.as_view()),
     path('delete/<int:id>/', views.delete_task),
     path('complete/<int:id>/', views.complete_task),
     path('api/tasks/', views.api_tasks),
--- todo_project/tasks/views.py
+++ todo_project/tasks/views.py
@@ -1,52 +1,41 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
-from .models import Task
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
+from .models import Task
 
-
-def register(request):
-    form = UserCreationForm(request.POST or None)
-    if form.is_valid():
-        user = form.save()
-        login(request, user)
-        return redirect('/')
-    return render(request, 'tasks/register.html', {'form': form})
-
-
-@login_required
 def task_list(request):
     filter_type = request.GET.get('filter', 'all')
 
-    tasks = Task.objects.filter(user=request.user)
+    tasks = Task.objects.all()
 
     if filter_type == 'active':
         tasks = tasks.filter(completed=False)
     elif filter_type == 'done':
         tasks = tasks.filter(completed=True)
 
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+
+        return redirect('/')
+
     return render(request, 'tasks/index.html', {'tasks': tasks})
 
 
-@login_required
 def delete_task(request, id):
-    task = Task.objects.get(id=id, user=request.user)
+    task = get_object_or_404(Task, id=id)
     task.delete()
     return redirect('/')
 
-
-@login_required
-def api_tasks(request):
-    tasks = Task.objects.filter(user=request.user)
-    data = list(tasks.values('id', 'title', 'completed'))
-    return JsonResponse(data, safe=False)
-
-from django.shortcuts import redirect, get_object_or_404
-from .models import Task
 
 def complete_task(request, id):
     task = get_object_or_404(Task, id=id)
     task.completed = True
     task.save()
     return redirect('/')
+
+
+def api_tasks(request):
+    tasks = Task.objects.all()
+    data = list(tasks.values('id', 'title', 'completed'))
+    return JsonResponse(data, safe=False)
